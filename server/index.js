0f4b263a1632342a42546ccfd81b8c59a2a5bd18
kinichahau87 <mmorfin0908@gmail.com>
2017-07-23 15:47:02 -0500

    added index.html

--- server/index.js
+++ server/index.js
@@ -1,23 +1,18 @@
 const express = require("express");
-const pool = require("../../kmSqlClient/src/dbManager").init("pool1");
-const trigModel = require("./models/trigtest.js");
+//const pool = require("../../kmSqlClient/src/dbManager").init("pool1");
+//const trigModel = require("./models/trigtest.js");
+const path = require("path");
 const app = express();
 
 
 app.get("/", function(req, res) {
-	var findPromise = trigModel.find();
-	findPromise.then(results => {
-		res.send(results);
-	}).catch(err => {
-		res.send(err);
-	});
+	res.sendFile(path.join(__dirname + "/public/html/index.html"));
+	
 });
 
 process.on("SIGTERM", function() {
-	console.log("shuting down")
-	pool.shutdown(function() {
-		process.exit();
-	});
+	console.log("shuting down");
+	
 });
 
 app.listen(3000, function() {
